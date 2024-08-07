dd84933b037d75610e3245c6178d513f4784c676
svetlana <zaxarovasweta@gmail.com>
2024-08-07 22:18:52 +0300

    Первое задание

---
+++ src/FirstClass.java
@@ -0,0 +1,132 @@
+import java.util.Arrays;
+public class FirstClass {
+    //1 метод
+    public void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    //2 метод
+    public void checkSumSign() {
+        int a = 5;
+        int b = 7;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    //3 метод
+    public void printColor() {
+        int value = 215;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+    //4 метод
+    public void compareNumbers() {
+        int a = 0;
+        int b = 4;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    //5 метод
+    public boolean checkDiapason(int a, int b) {
+        boolean flag = (a + b >= 10 && a + b <= 20);
+        System.out.println(flag);
+        return flag;
+    }
+    //6 метод
+    public void positiveNegative() {
+        int a = 9;
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+    //7 метод
+    public boolean numberPositive(int num) {
+        boolean isNumberPositive = (num >= 0);
+        System.out.println(isNumberPositive);
+        return isNumberPositive;
+    }
+    //8 метод
+    public void repeatWord(int a, String s) {
+        for (int i = 0; i < a; i++) {
+            System.out.print(s);
+        }
+        System.out.println();
+    }
+    //9 метод
+    public boolean leapYear() {
+        int year = 2004;
+            boolean flag = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+        System.out.println(flag);
+        return true;
+    }
+    // 10 метод
+    public void toggleArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 0, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else if (arr[i] == 1) {
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    //11 Метод
+    public void fillArray() {
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    //12 метод
+    public void multiplyArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    //13 метод
+    public void fillDoubleArray() {
+        int[][] a = new int[6][6];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (i == j || i + j == a.length - 1) {
+                    a[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(Arrays.toString(a[i]));
+        }
+    }
+    //14 метод
+    public int[] initArray(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FirstClass firstClass = new FirstClass();
+        //1
+        firstClass.printThreeWords();
+        //2
+        firstClass.checkSumSign();
+        //3
+        firstClass.printColor();
+        //4
+        firstClass.compareNumbers();
+        //5
+        firstClass.checkDiapason(13,15);
+        //6
+        firstClass.positiveNegative();
+        //7
+        firstClass.numberPositive(7);
+        //8
+        firstClass.repeatWord(5,"Method ");
+        //9
+        firstClass.leapYear();
+        //10
+        firstClass.toggleArray();
+        //11
+        firstClass.fillArray();
+        //12
+        firstClass.multiplyArray();
+        //13
+        firstClass.fillDoubleArray();
+        //14
+        firstClass.initArray(5,13);
     }
 }
