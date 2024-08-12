e4cc391a2130a9c8a3917307fe3894757b6745a3
svetlana <zaxarovasweta@gmail.com>
2024-08-13 00:09:45 +0300

    Первое задание

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+
 public class FirstClass {
+
     //1 метод
     public void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        System.out.println("Orange\nBanana\nApple");
     }
+
     //2 метод
     public void checkSumSign() {
         int a = 5;
@@ -16,19 +17,21 @@ public class FirstClass {
             System.out.println("Сумма отрицательная");
         }
     }
+
     //3 метод
     public void printColor() {
         int value = 215;
         if (value <= 0) {
             System.out.println("Красный");
         }
-        if (value > 0 && value <= 100) {
+        if (value <= 100) {
             System.out.println("Желтый");
         }
         if (value > 100) {
             System.out.println("Зеленый");
         }
     }
+
     //4 метод
     public void compareNumbers() {
         int a = 0;
@@ -39,12 +42,13 @@ public class FirstClass {
             System.out.println("a < b");
         }
     }
+
     //5 метод
     public boolean checkDiapason(int a, int b) {
         boolean flag = (a + b >= 10 && a + b <= 20);
-        System.out.println(flag);
         return flag;
     }
+
     //6 метод
     public void positiveNegative() {
         int a = 9;
@@ -54,12 +58,13 @@ public class FirstClass {
             System.out.println("Число отрицательное");
         }
     }
+
     //7 метод
     public boolean numberPositive(int num) {
-        boolean isNumberPositive = (num >= 0);
-        System.out.println(isNumberPositive);
-        return isNumberPositive;
+        boolean NumberPositive = (num >= 0);
+        return NumberPositive;
     }
+
     //8 метод
     public void repeatWord(int a, String s) {
         for (int i = 0; i < a; i++) {
@@ -67,13 +72,14 @@ public class FirstClass {
         }
         System.out.println();
     }
+
     //9 метод
     public boolean leapYear() {
         int year = 2004;
-            boolean flag = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
-        System.out.println(flag);
-        return true;
+        boolean flag = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+        return flag;
     }
+
     // 10 метод
     public void toggleArray() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 0, 0, 1};
@@ -86,6 +92,7 @@ public class FirstClass {
         }
         System.out.println(Arrays.toString(arr));
     }
+
     //11 Метод
     public void fillArray() {
         int[] arr = new int[100];
@@ -94,6 +101,7 @@ public class FirstClass {
         }
         System.out.println(Arrays.toString(arr));
     }
+
     //12 метод
     public void multiplyArray() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
@@ -104,6 +112,7 @@ public class FirstClass {
         }
         System.out.println(Arrays.toString(arr));
     }
+
     //13 метод
     public void fillDoubleArray() {
         int[][] a = new int[6][6];
@@ -118,6 +127,7 @@ public class FirstClass {
             System.out.println(Arrays.toString(a[i]));
         }
     }
+
     //14 метод
     public int[] initArray(int len, int initialValue) {
         int[] arr = new int[len];
